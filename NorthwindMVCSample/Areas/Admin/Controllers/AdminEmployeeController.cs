2f3af7c506098583822020804408c9dbca97b8bf
pelinisildar <pelincerav@gmail.com>
2019-11-03 12:46:26 +0300

    Employee Listeleme

---
+++ NorthwindMVCSample/Areas/Admin/Controllers/AdminEmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using NorthwindMVCSample.Models.ORM;
+using NorthwindMVCSample.Models.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthwindMVCSample.Areas.Admin.Controllers
+{
+    public class AdminEmployeeController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        // GET: Admin/AdminEmployee
+        public ActionResult Index()
+        {
+            List<EmployeeVM> model = db.Employees.Select(q => new EmployeeVM()
+            {
+                ID = q.EmployeeID,
+                FirstName = q.FirstName,
+                LastName = q.LastName,
+                Address=q.Address,                
+                Region = q.Region,
+                City = q.City
+            }).ToList();
+
+            return View(model);
+        }
+    }
+}
