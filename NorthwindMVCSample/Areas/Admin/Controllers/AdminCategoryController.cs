becd1e8e2bdc2a63d6a48db6a443d243c3641c22
Çağatay Yıldız <yildiz.cagatay@hotmail.com>
2019-11-03 12:17:50 +0300

    kategori listeleme, template

---
+++ NorthwindMVCSample/Areas/Admin/Controllers/AdminCategoryController.cs
@@ -0,0 +1,26 @@
+﻿using NorthwindMVCSample.Models.ORM;
+using NorthwindMVCSample.Models.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthwindMVCSample.Areas.Admin.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        public ActionResult Index()
+        {
+            List<CategoryVM> model = db.Categories.Select(q => new CategoryVM()
+            {
+                ID = q.CategoryID,
+                Name = q.CategoryName,
+                Description = q.Description
+            }).ToList();
+
+            return View(model);
+        }
+    }
+}
