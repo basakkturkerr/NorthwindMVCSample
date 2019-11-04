c290ab4e250ffbe580a6462c4ac419743701e86c
pelin cerav <pelin_crv_3417@hotmail.com>
2019-11-04 17:08:40 +0300

    adminuser list ekleme silme yapıldı. Detay yapılacak

---
+++ NorthwindMVCSample/Models/VM/AdminUserVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NorthwindMVCSample.Models.VM
+{
+    public class AdminUserVM
+    {
+        public int ID { get; set; }
+        public string EMail { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime? LastLoginDate { get; set; }
+    }
+}
